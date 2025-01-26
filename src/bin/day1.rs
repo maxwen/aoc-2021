775097c500a18feb5d7763ada54ae78af0072729
maxwen <max.weninger@gmail.com>
2025-01-26 12:27:51 +0100

    day1 - part1

---
+++ src/bin/day1.rs
@@ -0,0 +1,53 @@
+use aoc_2021::read_lines_as_vec;
+
+fn part1(lines: &[String]) -> u32 {
+    // 1226
+    let mut last = 0;
+    let mut sum = 0u32;
+    for line in lines.iter() {
+        let depth: u32 = line.parse().unwrap();
+        if last != 0 {
+            if depth > last {
+                sum += 1;
+            }
+        }
+        last = depth
+    }
+    sum
+}
+fn part2(lines: &[String]) -> u32 {
+    0u32
+}
+
+fn main() {
+    let lines = read_lines_as_vec("input/input_day1.txt").unwrap();
+
+    // let lines = vec![
+    //     "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
+    // ]
+    // .iter()
+    // .map(|s| s.to_string())
+    // .collect::<Vec<_>>();
+    println!("{}", part1(&lines));
+    println!("{}", part2(&lines));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let lines = vec![
+            "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
+        ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+
+        let result = part1(&lines);
+        assert_eq!(result, 7);
+        // let result = part2(&lines);
+        // assert_eq!(result, 45000);
+    }
+}
