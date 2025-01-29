d1f3481d4059227ecfab85bc5307d7b790be666c
maxwen <max.weninger@gmail.com>
2025-01-29 20:26:59 +0100

    day12 - part2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,7 +1,8 @@
 use aoc_2021::{read_lines_as_vec, Graph};
 use std::collections::{HashMap, HashSet, VecDeque};
 
-fn count_small_caves(path: &Vec<&str>) -> bool {
+// every small only once
+fn test_small_caves_once(path: &Vec<&str>) -> bool {
     let mut small_caves = HashSet::new();
     for p in path.iter() {
         let is_lowercase = p.chars().all(|c| c.is_lowercase());
@@ -15,19 +16,59 @@ fn count_small_caves(path: &Vec<&str>) -> bool {
     true
 }
 
-fn bfs(graph: &Graph<&str>, start: &str, end: &str) -> usize {
+fn has_any_small_cave_twice<'a>(small_caves: &'a Vec<&'a str>) -> Option<&'a str> {
+    for small_cave in small_caves.iter() {
+        let count = small_caves.iter().filter(|c| ***c == **small_cave).count();
+        if count == 2 {
+            return Some(small_cave);
+        }
+    }
+    None
+}
+// one small twice - rest only once
+fn test_small_caves_twice(path: &Vec<&str>) -> bool {
+    let mut small_caves: Vec<&str> = vec![];
+    for p in path.iter() {
+        let is_lowercase = p.chars().all(|c| c.is_lowercase());
+        if is_lowercase {
+            if let Some(twice_small_cave) = has_any_small_cave_twice(&small_caves) {
+                // would be the third time
+                if twice_small_cave == *p {
+                    return false;
+                }
+                // another cave is already in twice so we only allow this once
+                let count = small_caves.iter().filter(|c| ***c == **p).count();
+                if count == 0 {
+                    small_caves.push(p);
+                } else {
+                    return false;
+                }
+            } else {
+                small_caves.push(p);
+            }
+        }
+    }
+    true
+}
+
+fn bfs(
+    graph: &Graph<&str>,
+    start: &str,
+    end: &str,
+    small_cave_test: fn(path: &Vec<&str>) -> bool,
+) -> usize {
     let mut stack = VecDeque::new();
     stack.push_back(vec![start]);
 
     let mut seen: HashMap<&str, u32> = HashMap::new();
     seen.insert(start, 0);
 
-    let mut path_list = vec![];
+    let mut path_count = 0;
     while let Some(path) = stack.pop_front() {
         let tail = *path.last().unwrap();
         if tail == end {
             // println!("{:?}", path);
-            path_list.push(path);
+            path_count += 1;
             continue;
         }
 
@@ -37,13 +78,13 @@ fn bfs(graph: &Graph<&str>, start: &str, end: &str) -> usize {
                 let mut p = path.clone();
                 p.push(move_pos);
 
-                if count_small_caves(&path) {
+                if small_cave_test(&path) {
                     stack.push_back(p);
                 }
             }
         }
     }
-    path_list.len()
+    path_count
 }
 
 fn part1(lines: &[String]) -> usize {
@@ -61,11 +102,25 @@ fn part1(lines: &[String]) -> usize {
         graph.add_edge(n2, n1, 1);
     }
 
-    bfs(&graph, "start", "end")
+    bfs(&graph, "start", "end", test_small_caves_once)
 }
 
 fn part2(lines: &[String]) -> usize {
-    0usize
+    // 122880
+    let mut graph: Graph<&str> = Graph {
+        nodes: HashMap::new(),
+    };
+
+    for line in lines.iter() {
+        let parts = line.split("-").collect::<Vec<_>>();
+        let n1 = parts.first().unwrap();
+        let n2 = parts.last().unwrap();
+
+        graph.add_edge(n1, n2, 1);
+        graph.add_edge(n2, n1, 1);
+    }
+
+    bfs(&graph, "start", "end", test_small_caves_twice)
 }
 
 fn main() {
@@ -92,7 +147,7 @@ mod tests {
 
         let result = part1(&lines);
         assert_eq!(result, 10);
-        // let result = part2(&lines);
-        // assert_eq!(result, 195);
+        let result = part2(&lines);
+        assert_eq!(result, 36);
     }
 }
