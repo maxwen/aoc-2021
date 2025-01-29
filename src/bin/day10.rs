ecdbc129fd93280461d666e78af0f296ec1c86f5
maxwen <max.weninger@gmail.com>
2025-01-29 15:02:33 +0100

    day10 - part2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,19 +1,17 @@
-use std::collections::VecDeque;
 use aoc_2021::read_lines_as_vec;
+use std::collections::VecDeque;
 
 fn part1(lines: &[String]) -> u32 {
     // 321237
     let mut sum = 0;
     for line in lines.iter() {
         let mut stack = VecDeque::new();
-        let mut error_pos = 0;
         let mut error_type: char = 'c';
-        for (x, c) in line.chars().enumerate() {
+        for c in line.chars() {
             match c {
                 '(' => stack.push_front('('),
                 ')' => {
                     if stack.pop_front().unwrap() != '(' {
-                        error_pos = x;
                         error_type = ')';
                         break;
                     }
@@ -21,7 +19,6 @@ fn part1(lines: &[String]) -> u32 {
                 '[' => stack.push_front('['),
                 ']' => {
                     if stack.pop_front().unwrap() != '[' {
-                        error_pos = x;
                         error_type = ']';
                         break;
                     }
@@ -29,7 +26,6 @@ fn part1(lines: &[String]) -> u32 {
                 '{' => stack.push_front('{'),
                 '}' => {
                     if stack.pop_front().unwrap() != '{' {
-                        error_pos = x;
                         error_type = '}';
                         break;
                     }
@@ -38,7 +34,6 @@ fn part1(lines: &[String]) -> u32 {
 
                 '>' => {
                     if stack.pop_front().unwrap() != '<' {
-                        error_pos = x;
                         error_type = '>';
                         break;
                     }
@@ -59,10 +54,74 @@ fn part1(lines: &[String]) -> u32 {
     sum
 }
 
-fn part2(lines: &[String]) -> u32 {
-    for line in lines.iter() {}
+fn part2(lines: &[String]) -> usize {
+    // 2360030859
+    let mut score_list: Vec<usize> = vec![];
+    for line in lines.iter() {
+        let mut stack = VecDeque::new();
+        let mut error_type: char = '-';
+        for c in line.chars() {
+            match c {
+                '(' => stack.push_front('('),
+                ')' => {
+                    if stack.pop_front().unwrap() != '(' {
+                        error_type = ')';
+                        break;
+                    }
+                }
+                '[' => stack.push_front('['),
+                ']' => {
+                    if stack.pop_front().unwrap() != '[' {
+                        error_type = ']';
+                        break;
+                    }
+                }
+                '{' => stack.push_front('{'),
+                '}' => {
+                    if stack.pop_front().unwrap() != '{' {
+                        error_type = '}';
+                        break;
+                    }
+                }
+                '<' => stack.push_front('<'),
 
-    0u32
+                '>' => {
+                    if stack.pop_front().unwrap() != '<' {
+                        error_type = '>';
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if error_type == '-' {
+            let mut complete_line = "".to_string();
+            while let Some(c) = stack.pop_front() {
+                match c {
+                    '(' => complete_line += ')'.to_string().as_str(),
+                    '[' => complete_line += ']'.to_string().as_str(),
+                    '{' => complete_line += '}'.to_string().as_str(),
+                    '<' => complete_line += '>'.to_string().as_str(),
+                    _ => {}
+                }
+            }
+            let mut score = 0;
+            for c in complete_line.chars() {
+                match c {
+                    ')' => score = score * 5 + 1,
+                    ']' => score = score * 5 + 2,
+                    '}' => score = score * 5 + 3,
+                    '>' => score = score * 5 + 4,
+                    _ => {}
+                }
+            }
+            score_list.push(score);
+        }
+    }
+
+    score_list.sort();
+    score_list[score_list.len() / 2]
 }
 
 fn main() {
@@ -89,7 +148,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
 
     #[test]
     fn it_works() {
@@ -111,7 +170,7 @@ mod tests {
 
         let result = part1(&lines);
         assert_eq!(result, 26397);
-        // let result = part2(&lines);
-        // assert_eq!(result, 1134);
+        let result = part2(&lines);
+        assert_eq!(result, 288957);
     }
 }
